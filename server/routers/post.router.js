f36b009a246b40539dfd5e83ca1c2816639d8699
mariahelenanoren <marialinder97@hotmail.com>
2021-04-22 12:01:18 +0200

    fixed small bugs

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -10,7 +10,7 @@ router.get("/api/post", async (req, res) => {
 router.post("/api/post", async (req, res) => {
   const user = req.session.user;
   if (!user) {
-    return status(401).json("You are must login to create a post");
+    return res.status(401).json("You are must login to create a post");
   }
   const body = { ...req.body, user: req.session.user };
   const doc = await PostModel.create(body);
