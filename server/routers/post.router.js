b65e20036c2e279a2c2d0b16f37bb50479c9562e
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-04-22 15:18:03 +0200

    installed cookie-session and added doc comments

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -10,21 +10,21 @@ router.get("/api/post", async (req, res) => {
 router.post("/api/post", async (req, res) => {
   const user = req.session.user;
   if (!user) {
-    return res.status(401).json("You are must login to create a post");
+    return res.status(401).json("You must login to create a post");
   }
   const body = { ...req.body, user: req.session.user };
   const doc = await PostModel.create(body);
-  res.status(201).json(doc);
+  res.status(201).json("Post created");
 });
 
 router.put("/api/post", async (req, res) => {
   const doc = await PostModel.updateOne(req.body.id);
-  res.status(201).json(doc);
+  res.status(200).json("Update post successful");
 });
 
 router.delete("/api/post", async (req, res) => {
   const doc = await PostModel.deleteOne(req.body.id);
-  res.status(201).json(doc);
+  res.status(200).json("Delete post successful");
 });
 
 module.exports = router;
