47e98085a4a1e32070c3dadd68620219b0fb2847
mariahelenanoren <marialinder97@hotmail.com>
2021-04-22 15:32:14 +0200

    only admin and session user can edit or remove posts

--- server/routers/post.router.js
+++ server/routers/post.router.js
@@ -7,23 +7,39 @@ router.get("/api/post", async (req, res) => {
   res.status(200).json(docs);
 });
 
+router.get("/api/post/:id", async (req, res) => {
+  const { _id } = req.body;
+  const docs = await PostModel.find({ _id: _id });
+  res.status(200).json(docs);
+});
+
 router.post("/api/post", async (req, res) => {
-  const user = req.session.user;
+  const { user } = req.session;
   if (!user) {
-    return res.status(401).json("You are must login to create a post");
+    return res.status(401).json("You must login to create a post");
   }
   const body = { ...req.body, user: req.session.user };
   const doc = await PostModel.create(body);
   res.status(201).json(doc);
 });
 
-router.put("/api/post", async (req, res) => {
-  const doc = await PostModel.updateOne(req.body.id);
+router.use((req, res, next) => {
+  const { user, role } = req.session;
+  if (req.user !== user && role !== "admin") {
+    return res.status(403).json("Access denied");
+  }
+  next();
+});
+
+router.put("/api/post/:id", async (req, res) => {
+  const { _id, text } = req.body;
+  const doc = await PostModel.updateOne({ _id: _id }, { text: text });
   res.status(201).json(doc);
 });
 
-router.delete("/api/post", async (req, res) => {
-  const doc = await PostModel.deleteOne(req.body.id);
+router.delete("/api/post/:id", async (req, res) => {
+  const { _id } = req.body;
+  const doc = await PostModel.deleteOne({ _id: _id });
   res.status(201).json(doc);
 });
 
