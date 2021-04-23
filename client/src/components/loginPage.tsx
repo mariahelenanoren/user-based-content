47a201a066aa6659c0b888d24bac0d9814e718d5
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-04-23 11:11:49 +0200

    component loginPage basic layout

---
+++ client/src/components/loginPage.tsx
@@ -0,0 +1,16 @@
+import { CSSProperties } from "react";
+
+
+
+export default function LoginPage(){
+return (
+    <div style={mainStyle}>
+    </div>
+   );
+};
+
+const mainStyle: CSSProperties = {   
+         display: 'flex',
+         flexGrow: 1,
+         flexBasis: 0,
+    };
