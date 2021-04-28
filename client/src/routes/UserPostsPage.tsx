3cd32086c56dd9602ced71f64051b94006bdf00b
jenerlov <jenerlo@gmail.com>
2021-04-28 11:13:04 +0200

    modal

--- client/src/routes/UserPostsPage.tsx
+++ client/src/routes/UserPostsPage.tsx
@@ -1,9 +1,13 @@
+import Modal from "../components/Modal";
 import React, { CSSProperties, useEffect, useState } from "react";
 import Header from "../components/Header";
 import PostCard from "../components/postCard";
 import { makeRequest } from "../helper";
 
+
 export default function UserPostsPage() {
+  const [isModalVisible, setModalIsVisible] = useState(false);
+  // const [isEditModalVisible, setEditModalVisible] = useState(false);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -16,8 +20,9 @@ export default function UserPostsPage() {
 
   return (
     <>
-      <Header title={"Dina posts"} postButton={true} />
+      <Header title={"Dina posts"} setModalIsVisible={setModalIsVisible} postButton={true} />
       <div className="content" style={content}>
+      {isModalVisible && <Modal setModalIsVisible={setModalIsVisible}  />}
         {posts.map((post, id) => (
           <PostCard key={id} post={post} />
         ))}
