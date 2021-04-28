38b0cb9e87e6ff8c11d8e6a6b492813ef8b42c7c
mariahelenanoren <marialinder97@hotmail.com>
2021-04-28 11:36:37 +0200

    added endpoint for user status and role, and redirects if not authorized

--- client/src/routes/UserRoutes.tsx
+++ client/src/routes/UserRoutes.tsx
@@ -1,16 +1,31 @@
-import { CSSProperties } from "react";
-import { Route, Switch } from "react-router-dom";
+import { CSSProperties, useEffect, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { makeRequest } from "../helper";
 import AdminUsersPage from "./AdminUsersPage";
 import LatestPostPage from "./LatestPostsPage";
 import UserPostsPage from "./UserPostsPage";
 
 export default function UserRoutes() {
+  const [authentication, setAuthentication] = useState("LOGGED_IN");
+
+  useEffect(() => {
+    const getUserAuthentication = async () => {
+      const authentication = await makeRequest("/api/user-role", "GET");
+      setAuthentication(authentication);
+    };
+    getUserAuthentication();
+  }, []);
+
   return (
     <div style={main}>
       <Switch>
         <Route exact path={"/user"} component={UserPostsPage} />
         <Route path={"/user/latest-posts"} component={LatestPostPage} />
-        <Route path={"/user/users"} component={AdminUsersPage} />
+        {authentication === "admin" ? (
+          <Route path={"/user/users"} component={AdminUsersPage} />
+        ) : (
+          <Redirect to={"/user"} />
+        )}
       </Switch>
     </div>
   );
--- client/src/routes/layout.tsx
+++ client/src/routes/layout.tsx
@@ -1,34 +1,49 @@
-import { CSSProperties } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import RegistrationPage from './registrationPage';
-import LoginPage from './loginPage';
-import UserPage from './UserPage';
+import React, { CSSProperties, useEffect, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import RegistrationPage from "./registrationPage";
+import LoginPage from "./loginPage";
+import UserPage from "./UserPage";
+import { makeRequest } from "../helper";
 
 interface Props {}
 
 const Layout: React.FC<Props> = () => {
-   return (
-      <div style={ mainStyle }>
-         <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/registration" component={RegistrationPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/user"  component={UserPage} />
-         </Switch>
-      </div>
-   );
+  const [authentication, setAuthentication] = useState("LOGGED_IN");
+
+  useEffect(() => {
+    const getUserAuthentication = async () => {
+      const authentication = await makeRequest("/api/user-status", "GET");
+      setAuthentication(authentication);
+    };
+    getUserAuthentication();
+  }, []);
+
+  return (
+    <div style={mainStyle}>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/registration" component={RegistrationPage} />
+        <Route path="/login" component={LoginPage} />
+        {authentication === "LOGGED_IN" ? (
+          <Route path="/user" component={UserPage} />
+        ) : (
+          <Redirect to={"/"} />
+        )}
+      </Switch>
+    </div>
+  );
 };
 
 export default Layout;
 
 const mainStyle: CSSProperties = {
-   display: 'flex',
-   flexDirection: 'column',
-   width: '100%',
-   height: '100vh',
-   justifyContent: 'center',
-   alignItems: 'center',
-   textAlign: 'center',
-   backgroundColor: '#1f1f1f',
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  height: "100vh",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  backgroundColor: "#1f1f1f",
 };
